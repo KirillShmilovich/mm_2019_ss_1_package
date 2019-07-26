2bbb512d156314776e45b206fa5f433e7f01f5ac
Yutao Ma <yma3@uchicago.edu>
2019-07-26 16:38:03 -0500

    Editedt initialization in geom

--- mm_2019_ss_1_package/geom.py
+++ mm_2019_ss_1_package/geom.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 class Geom:
-    def __init__(self, method, num_particles, box_length, file_name = None):
-        self.method = method
-        self.file_name = file_name
-        self.num_particles = num_particles
-        self.box_length = box_length
-        self.volume = np.power(self.box_length,3)
-        self.generate_initial_state()
+    #def __init__(self, method, num_particles, box_length, file_name = None):
+    def __init__(self, method, **kwargs):
+        #self.method = method
+        # self.file_name = file_name
+        # self.num_particles = num_particles
+        # self.box_length = box_length
+        # self.volume = np.power(self.box_length,3)
+        self.generate_initial_state(method,**kwargs)
 
-    def generate_initial_state(self):
+    def generate_initial_state(self,method,**kwargs):
         """Generate initial coordinates of particles in a box either randomly or based on a file.
 
         Parameters
@@ -29,10 +30,27 @@ class Geom:
             Array of particle coordinates generated for an initial state
         """
 
-        if self.method is 'random':
+        if method is 'random':
+            if ('num_particles' not in kwargs || 'box_length' not in kwargs):
+                raise ValueError(' "num_particles" and "box_length" arguments must be set for method=random!')
+            self.num_particles = kwargs['num_particles']
+            self.box_length = kwargs['box_length']
+            self.volume = self.box_length**3
             self.coordinates = (0.5 - np.random.rand(self.num_particles, 3)) * self.box_length
-        elif self.method is 'file':
+
+        elif method is 'file':
+            if ('file_name' not in kwargs):
+                raise ValueError('"filename" argument must be set for method = file!')
+            file_name = kwargs['file_name']
+            with open(file_name) as f:
+                lines = f.readlines()
+                self.box_length = np.fromstring(lines[0], dtype=float, sep=',')[0]
+                self.volume = self.box_length**3
+                self.num_particles = np.fromstring(lines[1], dtype=float, sep=',')[0]
             self.coordinates = np.loadtxt(self.file_name, skiprows=2, usecols=(1,2,3))
+            if (self.num_particles != self.coordinates.shape[0]):
+                raise ValueError('Inconsistent value of number of particles in file!')
+
         else:
             raise TypeError('Method type not recognized.')
 
@@ -58,17 +76,17 @@ class Geom:
         rij2 = np.sum(rij**2,axis=1)
         return rij2
 
-    def wrap(self,v):
-        """Wrap a vector (or set of vectors) as np array into periodic box
+    def wrap(self):
+        """Wrap all coordiantes back to periodic box
 
         Parameters
         ----------
-        v : the vector (or set of vectors) as np array to be wrapped
+        None
 
         Returns
         -------
-        v_wrapped: the wrapped vector (or set of vectors) as an np array
+        None
 
         """
-        v_wrapped = v - self.box_length*np.round(v/self.box_length)
-        return v_wrapped
+        v_wrapped = self.coords - self.box_length*np.round(self.coords/self.box_length)
+
