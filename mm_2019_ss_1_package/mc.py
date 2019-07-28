72d564e4a12837b5bdb84c7b1340203b74df10f1
Yutao Ma <yma3@uchicago.edu>
2019-07-27 22:14:45 -0500

    Fixed typo in MC.py

--- mm_2019_ss_1_package/mc.py
+++ mm_2019_ss_1_package/mc.py
@@ -1,8 +1,6 @@
 import numpy as np
-#from .geom import Geom
-#from .energy import Energy
-from geom import Geom
-from energy import Energy
+from .geom import Geom
+from .energy import Energy
 class MC:
 
     def __init__(self, method, reduced_temp, max_displacement, cutoff, num_particles = None, file_name = None, tune_displacement = True, reduced_den = None):
@@ -21,9 +19,6 @@ class MC:
             raise ValueError("Method must be either 'file' or 'random'")
 
         self._Energy = Energy(self._Geom, cutoff)
-        
-        self.tail_correction = self._Energy.calculate_tail_correction()
-        self.total_pair_energy = self._Energy.calculate_total_pair_energy()
 
     def _accept_or_reject(self,delta_e):
         if delta_e < 0.0:
@@ -61,9 +56,12 @@ class MC:
         import os.path
         if (not os.path.exists(save_dir)):
             raise ValueError("Snapshot saving directory does not exist!")
-        self._energy_array = np.zeros(n_steps)
+        self._energy_array = np.zeros(n_steps+1)
 
-        for i_step in range(n_steps):
+        tail_correction = self._Energy.calculate_tail_correction()
+        total_pair_energy = self._Energy.calculate_total_pair_energy()
+        self._energy_array[0] = total_pair_energy
+        for i_step in range(1,n_steps+1):
             self._n_trials += 1
             i_particle = np.random.randint(self._Geom.num_particles)
             random_displacement = (2.0 * np.random.rand(3) - 1.0) * self.max_displacement
@@ -76,12 +74,12 @@ class MC:
             accept = self._accept_or_reject(delta_e)
 
             if accept:
-                self.total_pair_energy += delta_e
+                total_pair_energy += delta_e
                 self._n_accept += 1
             else:
                 self._Geom.coordinates[i_particle,:] = old_coordinate
 
-            total_energy = (self.total_pair_energy + self.tail_correction) / self._Geom.num_particles
+            total_energy = (total_pair_energy + tail_correction) / self._Geom.num_particles
             self._energy_array[i_step] = total_energy
 
             if np.mod(i_step + 1, freq) == 0:
@@ -91,5 +89,9 @@ class MC:
                     self._adjust_displacement()
 
 if __name__ == "__main__":
-    sim = MC(method = 'random', num_particles = 100, reduced_den = 0.9, reduced_temp = 0.9, max_displacement = 0.1, cutoff = 3.0)
-    sim.run(n_steps = 50000, freq = 1000)
+    import time
+    start = time.time()
+    sim = MC(method = 'random', num_particles = 1000, reduced_den = 0.9, reduced_temp = 0.9, max_displacement = 0.1, cutoff = 3.0)
+    sim.run(n_steps = 500000, freq = 10000)
+    end = time.time()
+    print ("Sim takes %10.5f seconds"%(end-start))
