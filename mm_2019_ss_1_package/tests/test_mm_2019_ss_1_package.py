0f5558aa8d6ebcf80046ec0dd0b1533e4509bdc8
Hanbo Hong <hhong11@ucmerced.edu>
2019-07-28 02:07:28 -0500

    add unit test for calculate_minimum_image_distance

--- mm_2019_ss_1_package/tests/test_mm_2019_ss_1_package.py
+++ mm_2019_ss_1_package/tests/test_mm_2019_ss_1_package.py
@@ -6,7 +6,35 @@ Unit and regression test for the mm_2019_ss_1_package package.
 import mm_2019_ss_1_package
 import pytest
 import sys
+import numpy as np
+
+@pytest.fixture()
+def trial_sim():
+    method = "random"
+    num_particles = 1000
+    reduced_den = 1.0
+    reduced_temp = 1.0
+    max_displacement = 0.1
+    cutoff = 3.0
+
+    trial_sim = mm_2019_ss_1_package.MC(method = method, num_particles = num_particles, reduced_den = reduced_den, reduced_temp = reduced_temp, max_displacement = max_displacement, cutoff = cutoff)
+
+    return trial_sim
 
 def test_mm_2019_ss_1_package_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mm_2019_ss_1_package" in sys.modules
+
+
+def test_minimum_image_distance(trial_sim):
+    """Test the method to calculate minimum image distance. Arguments for MC.py are set to be easy ones so that expected value is 2"""
+
+    trial_point1 = np.array([1,0,0])
+    trial_point2 = np.array([9,0,0])
+        
+    calculated_value = trial_sim.Geom.minimum_image_distance(trial_point1 , trial_point2)
+    expected_value = 2.0 ** 2
+
+    assert np.isclose(calculated_value , expected_value)
+
+    
