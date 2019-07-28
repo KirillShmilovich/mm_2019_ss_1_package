0ed46cff1f3d4482c3635f5daade091dcd7879bc
Kirill Shmilovich <kshmilov@gmail.com>
2019-07-28 10:19:25 -0500

    changed fixture to take arguments

--- mm_2019_ss_1_package/tests/test_mm_2019_ss_1_package.py
+++ mm_2019_ss_1_package/tests/test_mm_2019_ss_1_package.py
@@ -10,16 +10,12 @@ import numpy as np
 
 @pytest.fixture()
 def trial_sim():
-    method = "random"
-    num_particles = 1000
-    reduced_den = 1.0
-    reduced_temp = 1.0
-    max_displacement = 0.1
-    cutoff = 3.0
+    # Changed the fixture to so we can pass arguments
+    def _get_trial_sim(method = 'random', num_particles = 1000, reduced_den = 1.0, reduced_temp = 1.0, max_displacement = 0.1, cutoff = 3.0):
+        trial_sim = mm_2019_ss_1_package.MC(method = method, num_particles = num_particles, reduced_den = reduced_den, reduced_temp = reduced_temp, max_displacement = max_displacement, cutoff = cutoff)
+        return trial_sim
 
-    trial_sim = mm_2019_ss_1_package.MC(method = method, num_particles = num_particles, reduced_den = reduced_den, reduced_temp = reduced_temp, max_displacement = max_displacement, cutoff = cutoff)
-
-    return trial_sim
+    return _get_trial_sim
 
 def test_mm_2019_ss_1_package_imported():
     """Sample test, will always pass so long as import statement worked"""
@@ -28,13 +24,11 @@ def test_mm_2019_ss_1_package_imported():
 
 def test_minimum_image_distance(trial_sim):
     """Test the method to calculate minimum image distance. Arguments for MC.py are set to be easy ones so that expected value is 2"""
-
+    sim = trial_sim()
     trial_point1 = np.array([1,0,0])
     trial_point2 = np.array([9,0,0])
         
-    calculated_value = trial_sim.Geom.minimum_image_distance(trial_point1 , trial_point2)
+    calculated_value = sim.Geom.minimum_image_distance(trial_point1 , trial_point2)
     expected_value = 2.0 ** 2
 
     assert np.isclose(calculated_value , expected_value)
-
-    
